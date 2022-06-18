adf7880f7975abaa3cbff10736e442b66fff4f5c
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-19 07:00:00 +1200

    completed exercise 6.9

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
 
-// creation of a store from a reducer
-const store = createStore(reducer)
+import store from './store'
+
+// // creation of a store from a reducer
+// const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // This is where the link between the store and the application is made. Via the `Provider` component of `react-redux`
