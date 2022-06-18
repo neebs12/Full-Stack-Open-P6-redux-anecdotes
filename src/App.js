9d5a17d45a4fe774edf5d923601d27450cf13132
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-19 06:08:05 +1200

    white spaces

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux'
-
-import { incrementVote, addAnecdote } from './reducers/anecdoteReducer'
-
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
   // these are present here because <Provider store={store}>...</Provider>
-  // useSelector essentially fetches the state then passes it through a function that processes it (typically filters with minor transformations?)
 
   return (
     <div>
